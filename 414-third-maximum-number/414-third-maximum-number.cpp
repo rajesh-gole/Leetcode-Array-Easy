3b623c2fc1fbe475d1c779b9724e64beb2d94e6f
RAJESH GOLE <44594864+rajesh-gole@users.noreply.github.com>
2022-06-23 18:30:47 +0530

    Time: 19 ms (15.26%), Space: 10.1 MB (39.08%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        int _max = 0;
         
-        unordered_set<int> seen{nums.begin(), nums.end()};
-        
-        if(seen.size() < 3) return *max_element(seen.begin(), seen.end());
-        
-        for(int i = 0; i < 3; ++i) {
-            auto mx = max_element(seen.begin(), seen.end());
-            _max = *mx; 
-            seen.erase(mx);
-        } 
-        
-        return _max;
-    }
-};
-
-//rajesh-gole
-
-/*
-// Store all elements to set s
-    
+    // Store all elements to set s
     unordered_set<int> s{nums.begin(), nums.end()};
     
     int res=0;
     
     unordered_set<int>::iterator it;
+
+    // If the third maximum does not exist, return the maximum number. 
+    if(s.size() < 3) return *max_element(s.begin(), s.end());
     
     // deleting maximum number upto 3rd max
     for(int i=0;i<3;i++)
@@ -38,4 +22,10 @@ public:
     }
 
     return res;
-*/
+
+    }
+};
+
+//rajesh-gole
+
+
