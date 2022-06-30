932c23beb9b3a2ecbde7c1ecaf3134586d2009e3
RAJESH GOLE <44594864+rajesh-gole@users.noreply.github.com>
2022-06-30 12:04:32 +0530

    Time: 3 ms (56.58%), Space: 6.5 MB (21.58%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.cpp
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     string truncateSentence(string s, int k) {
-        stringstream ss(s);
-        string word;
-        string res;
-        int count=0;
-        while(ss>>word)
-        {
-            res=res+word;
-            count++;
-            if(count==k)
-                break;
-            res+=' ';
-        }
-        return res;
+        
+    string ans;
+    int n=s.size(),count=k;
+    
+    for(int i=0;i<n && k>0;i++)
+    {
+     
+        if(s[i]==' ')
+            --k;
+        if(k>0)
+            ans+=s[i];
+
+    }    
+    return ans;
     }
 };
 
+// Without Stringstream
 
 
 //rajesh-gole
